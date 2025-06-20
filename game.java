9e15562a1368a6a195730ffda8d2857e3f9bc8b3
Rishi R <rishirreddy18@gmail.com>
2025-06-20 12:15:09 +0000

    game

--- game.java
+++ game.java
@@ -13,12 +13,43 @@ public class game{
         String ComputersChoice;
         String PlayAgain = "yes";
 
-        System.out.print("Enter your Choice (rock, paper,scissor) = ");
+        do {
+            System.out.print("Enter your Choice (rock, paper,scissor) : ");
         playersChoice = scanner.nextLine();
+
+        ComputersChoice = choices[random.nextInt(3)];
+        System.out.println("Computers Choice: " + ComputersChoice);
 
         if (!playersChoice.equals("rock")&& !playersChoice.equals("paper")&& !playersChoice.equals("scissor")) {
             System.out.println("Invalid Choice");
+            continue;
         }
+
+        if (playersChoice.equals(ComputersChoice)) {
+            System.out.println("Its a tie");
+        }
+        else if(playersChoice.equals("rock") && ComputersChoice.equals("scissor")){
+            System.out.println("you win");
+
+        }
+        else if(playersChoice.equals("paper") && ComputersChoice.equals("scissor")){
+            System.out.println("you win");
+        }
+        else if(playersChoice.equals("scissor") && ComputersChoice.equals("paper")){
+            System.out.println("you win");
+        }
+        else{
+            System.out.println("you loose");
+        }
+
+        System.out.println("Play Again (yes/no): ");
+        PlayAgain = scanner.nextLine().toLowerCase();
+            
+        } while (PlayAgain.equals("yes"));
+
+        
+
+
 
     }
 }
